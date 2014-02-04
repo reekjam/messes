17d912e5a33afb9565d3f949d436d3980f475ac1
Rick Rieta <rrieta@biddingforgood.com>
2014-02-04 11:09:45 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messes::Application.config.secret_key_base = '76bf77026a03bad2fc4d06aa3aa5b16429aa26c21841d7a20b8b1129799851b36777b40806f6f680feba7095294b80a93a9486a079b9a34cf0b6ea66218a2f40'
