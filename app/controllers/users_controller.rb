ccd13d54636c334522cb482eddc13fd93f61a94e
rickjcr3 <rickrieta@gmail.com>
2014-02-09 14:37:52 -0500

    Set up photo partial for user feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def index
     client = Instagram.client(:access_token => session[:access_token])
     @user = client.user
+    @photos = client.user_recent_media
+
+    @months = Date::ABBR_MONTHNAMES
   end
 end
