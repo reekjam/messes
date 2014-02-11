7f558750a2558c695150190af67c01112acb41f7
rickjcr3 <rickrieta@gmail.com>
2014-02-10 22:12:25 -0500

    Add logic to dropdown selection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   def index
     client = Instagram.client(:access_token => session[:access_token])
     @user = client.user
-    @photos = client.user_recent_media
-
+    @photos = client.user_recent_media(count: -1)
     @months = Date::ABBR_MONTHNAMES
+    @years = Array.new
+    @photos.each do |photo|
+      @years << DateTime.strptime( photo[:created_time].to_s, '%s' ).year
+    end
+    @years = @years.uniq!
   end
 end
