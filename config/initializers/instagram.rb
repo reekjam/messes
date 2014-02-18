296af090e41c5de8b008589db56fcee6b9ff824d
rickjcr3 <rickrieta@gmail.com>
2014-02-18 18:51:23 -0500

    Fix merge conflicts

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,16 +1,5 @@
 require 'instagram'
 
-<<<<<<< HEAD
-CALLBACK_URL = 'http://lvh.me:3000/oauth/callback/'
-
 Instagram.configure do |config|
-  config.client_id = "76c97b573e6b444c857254a795925444"
-  config.client_secret = "adcab0f7e31345a58b4594e08566d255"
-=======
-CALLBACK_URL = "http://lvh.me:3000/oauth/callback/"
-
-Instagram.configure do |config|
-  config.client_id = '76c97b573e6b444c857254a795925444'
-  config.client_secret = 'adcab0f7e31345a58b4594e08566d255'
->>>>>>> 0d244ef12bf963242a21a438f77b449da4ab90fa
-end
+  config.client_id = ENV['IG_CLIENT_ID']
+  config.client_secret = ENV['IG_CLIENT_SECRET']
