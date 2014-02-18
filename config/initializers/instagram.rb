d0bc67a7e06a1da4f78ab54cb099171e1aa85faa
Rick Rieta <rrieta@biddingforgood.com>
2014-02-18 15:21:27 -0500

    Update gitignore

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -3,6 +3,6 @@ require 'instagram'
 CALLBACK_URL = "http://lvh.me:3000/oauth/callback/"
 
 Instagram.configure do |config|
-  config.client_id = ENV["IG_CLIENT_ID"]
-  config.client_secret = ENV["IG_CLIENT_SECRET"]
+  config.client_id = d90c75a1e8d1440c9ea8bdad2e31e52d
+  config.client_secret = 9c75461600594cae993f424424bfc79e
 end
