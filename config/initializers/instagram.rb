32f9811b6b573919c40ec193d02e20deb1f3d02b
rickjcr3 <rickrieta@gmail.com>
2014-02-18 18:29:36 -0500

    Add missing end statement

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -5,3 +5,4 @@ CALLBACK_URL = 'http://lvh.me:3000/oauth/callback/'
 Instagram.configure do |config|
   config.client_id = "76c97b573e6b444c857254a795925444"
   config.client_secret = "adcab0f7e31345a58b4594e08566d255"
+end
