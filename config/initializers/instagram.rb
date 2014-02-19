eaed469a518cb3d62728809dab0dbb7d2ad8b223
rickjcr3 <rickrieta@gmail.com>
2014-02-18 20:18:36 -0500

    Add profile picture to navbar

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,9 +1,11 @@
 require 'instagram'
 
-CALLBACK_URL = ENV['CALLBACK_URL']
+CALLBACK_URL = 'http://lvh.me:3000/oauth/callback/'
+IG_CLIENT_ID = '76c97b573e6b444c857254a795925444'
+IG_CLIENT_SECRET = 'adcab0f7e31345a58b4594e08566d255'
 
 Instagram.configure do |config|
-  config.client_id = ENV['IG_CLIENT_ID']
-  config.client_secret = ENV['IG_CLIENT_SECRET']
+  config.client_id = IG_CLIENT_ID
+  config.client_secret = IG_CLIENT_SECRET
 end
 
