73532eb31513add956e84e6bd892728f385adbe2
rickjcr3 <rickrieta@gmail.com>
2014-08-19 18:34:33 -0400

    Add api key for development

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,7 +1,14 @@
 require 'instagram'
 
-Instagram.configure do |config|
-  config.client_id = ENV['IG_CLIENT_ID']
-  config.client_secret = ENV['IG_CLIENT_SECRET']
+if Rails.env == 'development'
+  Instagram.configure do |config|
+    config.client_id = '76c97b573e6b444c857254a795925444'
+    config.client_secret = 'adcab0f7e31345a58b4594e08566d255'
+  end
+else
+  Instagram.configure do |config|
+    config.client_id = ENV['IG_CLIENT_ID']
+    config.client_secret = ENV['IG_CLIENT_SECRET']
+  end
 end
 
