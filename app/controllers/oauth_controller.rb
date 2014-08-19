3cc0672abe18994b452ccd5dd174d9949b31c3f9
rickjcr3 <rickrieta@gmail.com>
2014-08-19 18:38:47 -0400

    Update oauth controller for development

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,10 +1,18 @@
 class OauthController < ApplicationController
   def connect
-    redirect_to Instagram.authorize_url(:redirect_uri => ENV['CALLBACK_URL'])
+    if Rails.env == 'development'
+      redirect_to Instagram.authorize_url(:redirect_uri => 'http://localhost:3000/oauth/callback')
+    else 
+      redirect_to Instagram.authorize_url(:redirect_uri => ENV['CALLBACK_URL'])
+    end
   end
 
   def callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => ENV['CALLBACK_URL'])
+    if Rails.env == 'development'
+      response = Instagram.get_access_token(params[:code], :redirect_uri => 'http://localhost:3000/oauth/callback')
+    else
+      response = Instagram.get_access_token(params[:code], :redirect_uri => ENV['CALLBACK_URL'])
+    end
     session[:access_token] = response.access_token
     redirect_to users_index_path
   end
