9237e288cec6a04ab8bbc129a562db61b19f2f25
rickjcr3 <rickrieta@gmail.com>
2014-08-28 23:58:48 -0400

    Add x to close modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
   $('.user-photo.th').click(function(){
     $('#overlay').fadeIn('fast');
     $('#modal').fadeIn('fast');
+    $('#close-btn').fadeIn('fast');
     photo_url = $(this).data('photo-url')
     document.getElementById('modal-photo').innerHTML = "<img src=" + photo_url + " height='612' width='612'>"
   })
 })
-$(document).on('click', '#overlay', function(){
+$(document).on('click', '#close-btn', function(){
+  $(this).fadeOut('fast');
   $('#overlay').fadeOut('fast');
   $('#modal').fadeOut('fast');
 })
