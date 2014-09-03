298d0a41cddb456337178f8fd47acabe2b308d43
rickjcr3 <rickrieta@gmail.com>
2014-09-03 19:57:21 -0400

    Add all relevant selectors on one line

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,25 +2,17 @@ $(document).ready(function(){
   var photo_url
   var caption
   $('.user-photo.th').click(function(){
-    $('#overlay').fadeIn('fast');
-    $('#modal').fadeIn('fast');
-    $('#close-btn').fadeIn('fast');
+    $('#overlay, #modal, #modal-caption, #close-btn').fadeIn('fast');
     photo_url = $(this).data('photo-url')
-    document.getElementById('modal-photo').innerHTML = "<img src=" + photo_url + " height='612' width='612'>"
     caption = $(this).children('.caption').text()
+    document.getElementById('modal-photo').innerHTML = "<img src=" + photo_url + " height='612' width='612'>"
     document.getElementById('modal-caption').innerHTML = caption
-    $('#modal-caption').fadeIn('fast')
-    $('#user-feed').addClass('blurry')
-    $('#background-img').addClass('blurry')
+    $('#user-feed, #background-img').addClass('blurry')
   })
 })
 $(document).on('click', '#close-btn', function(){
-  $(this).fadeOut('fast');
-  $('#overlay').fadeOut('fast');
-  $('#modal').fadeOut('fast');
-  $('#modal-caption').fadeOut('fast');
-  $('#user-feed').removeClass('blurry');
-  $('#background-img').removeClass('blurry');
+  $('this, #overlay, #modal, #modal-caption').fadeOut('fast');
+  $('#user-feed, #background-img').removeClass('blurry');
 })
 $(document).ready(function(){
   auto_height = $(window).height() - $('.top-bar').height()
