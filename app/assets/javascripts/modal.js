8129ba70ad43baf808453392d7a033806c187892
rickjcr3 <rickrieta@gmail.com>
2014-09-02 23:40:09 -0400

    Add photo caption to modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
   var photo_url
+  var caption
   $('.user-photo.th').click(function(){
     $('#overlay').fadeIn('fast');
     $('#modal').fadeIn('fast');
     $('#close-btn').fadeIn('fast');
     photo_url = $(this).data('photo-url')
     document.getElementById('modal-photo').innerHTML = "<img src=" + photo_url + " height='612' width='612'>"
+    caption = $(this).children('.caption').text()
+    document.getElementById('modal-caption').innerHTML = caption
+    $('#modal-caption').fadeIn('fast')
   })
 })
 $(document).on('click', '#close-btn', function(){
   $(this).fadeOut('fast');
   $('#overlay').fadeOut('fast');
   $('#modal').fadeOut('fast');
+  $('#modal-caption').fadeOut('fast');
 })
 $(document).ready(function(){
   auto_height = $(window).height() - $('.top-bar').height()
