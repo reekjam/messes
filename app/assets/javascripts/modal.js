d7995256bb77b8bd3e30a64ac94cbcf068dd5663
rickjcr3 <rickrieta@gmail.com>
2014-09-13 16:24:02 -0400

    Add a more reliable way to calculate the background image height for the container top margin

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,26 +1,25 @@
 $(document).ready(function(){
   var photo_url
   var caption
-  $('.user-photo.th').click(function(){
-    $('#overlay, #modal, #modal-caption, #close-btn').fadeIn('fast');
-    photo_url = $(this).data('photo-url')
-    caption = $(this).children('.caption').text()
-    document.getElementById('modal-photo').innerHTML = "<img src=" + photo_url + " height='612' width='612'>"
-    document.getElementById('modal-caption').innerHTML = caption
-    $('#user-feed, #background-img').addClass('blurry')
-  })
+  var win_width = $(window).width()
+  var container_top_margin = win_width * .5625
+  $('.container').css('top', container_top_margin)
+  console.log('container_top_margin:' + container_top_margin)
+  var user_feed_height = $(window).height() - $('.top-bar').height()
+  $('#user-feed').css('min-height', user_feed_height)
+  console.log('auto_height:' + user_feed_height)
+})
+$(document).on('click', '.user-photo.th', function(){
+  $('#overlay, #modal, #modal-caption, #close-btn').fadeIn('fast');
+  photo_url = $(this).data('photo-url')
+  caption = $(this).children('.caption').text()
+  document.getElementById('modal-photo').innerHTML = "<img src=" + photo_url + " height='612' width='612'>"
+  document.getElementById('modal-caption').innerHTML = caption
+  $('#user-feed, #background-img').addClass('blurry')
 })
 $(document).on('click', '#close-btn', function(){
   $('#close-btn, #overlay, #modal, #modal-caption').fadeOut('fast');
   $('#user-feed, #background-img').removeClass('blurry');
-})
-$(document).ready(function(){
-  user_feed_height = $(window).height() - $('.top-bar').height()
-  $('#user-feed').css('min-height', user_feed_height)
-  console.log('auto_height:' + user_feed_height)
-  container_top_margin = $('#background-img').height()
-  $('.container').css('top', container_top_margin)
-  console.log('container_top_margin:' + container_top_margin)
 })
 $(window).resize(function(){
   background_img_margin = $('#background-img').height()
